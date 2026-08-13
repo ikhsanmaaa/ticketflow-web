d8bb48253ca4b78357add6b4fe841b32c3e82c18
ikhsanmaulana <ikhsanmaaa@gmail.com>
2026-08-13 20:17:49 +0700

    fixing dialog tiket bug

---
+++ src/app/core/stores/ticket-stores.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, signal } from '@angular/core';
+
+import { Ticket } from '../models/ticket';
+import { TicketService } from '../services/ticket.service';
+import { TicketStorageService } from '../services/ticket-storage.sevice';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TicketStoreService {
+  constructor(
+    private readonly ticketService: TicketService,
+    private readonly ticketStorage: TicketStorageService,
+  ) {}
+
+  readonly tickets = signal<Ticket[]>([]);
+
+  readonly loading = signal(false);
+
+  readonly error = signal<string | null>(null);
+
+  readonly selectedTicket = signal<Ticket | null>(null);
+
+  readonly dialogOpen = signal(false);
+
+  select(ticket: Ticket) {
+    this.selectedTicket.set(ticket);
+    this.dialogOpen.set(true);
+  }
+
+  closeDialog() {
+    this.dialogOpen.set(false);
+    this.selectedTicket.set(null);
+  }
+
+  loadByToken(token: string) {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.ticketService.getTickets(token).subscribe({
+      next: (ticket) => {
+        const cachedTickets = this.ticketStorage.get();
+
+        const updatedTickets = this.mergeTickets(ticket, cachedTickets);
+
+        this.tickets.set(updatedTickets);
+
+        this.ticketStorage.save(updatedTickets);
+
+        this.loading.set(false);
+      },
+
+      error: () => {
+        this.loading.set(false);
+        this.error.set('Gagal memuat tiket');
+      },
+    });
+  }
+
+  load() {
+    const cachedTickets = this.ticketStorage.get();
+
+    this.tickets.set(cachedTickets);
+  }
+
+  private mergeTickets(ticket: Ticket, cachedTickets: Ticket[]): Ticket[] {
+    const filtered = cachedTickets.filter((item) => item.key !== ticket.key);
+
+    return [ticket, ...filtered];
+  }
+
+  clearHistory() {
+    this.ticketStorage.clear();
+    this.tickets.set([]);
+  }
+}
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, signal } from '@angular/core';
 
 import { HeroComponent } from './components/hero/hero.component';
 import { PrivacyCardComponent } from './components/privacy-card/privacy-card.component';
-import { TicketStoreService } from '@/core/stores/ticket-stores.service';
 import { TokenService } from '@/core/services/token.service';
 import { TicketSectionComponent } from './components/ticket-section/ticket-section.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { ActivatedRoute } from '@angular/router';
+import { TicketStoreService } from '@/core/stores/ticket-stores.service';
 
 @Component({
   standalone: true,
